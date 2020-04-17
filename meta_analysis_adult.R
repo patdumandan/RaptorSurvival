4f68ca1bf480942462d075796dade89636c3aa0d
patdumandan <46734193+patdumandan@users.noreply.github.com>
2020-04-17 10:17:57 -0600

    Update meta_analysis_adult.R

--- meta_analysis_adult.R
+++ meta_analysis_adult.R
@@ -13,12 +13,15 @@ survival=surv$survival.est
 mass=surv$Average.mass..kg.
 species=surv$spcode
 surv$spcode=as.integer(surv$Species)
+surv$stcode=as.integer(surv$Reference)
 Nsp=40
 death_type=as.integer(surv$death.type)
 str(surv)
+study=surv$Reference
+Nst=77
 
-#model code############
-surv_mod2=stan(model_code="
+
+surv_mod3=stan(model_code="
   
   data{
 
@@ -26,7 +29,9 @@ surv_mod2=stan(model_code="
   real <lower=0, upper=1> survival[N]; //survival estimate
   real <lower=0> mass[N];// ave.mass in kg
   int species[N]; //ID of each species
+  int study [N]; //ID of study
   int Nsp; //no.of species
+  int Nst; //no.of studies
   int death_type[N];// direct/indirect
 
                 }
@@ -35,6 +40,7 @@ surv_mod2=stan(model_code="
 
   real <lower=0> alpha;// global intercept
   real <lower=0> alpha_sp[Nsp]; //random intercept per species
+  real <lower=0> alpha_st [Nst];// random intercept per study
   real <lower=0> beta1; //slope age
   real <lower=0> beta2; //slope indirect effect
   real<lower=0> sigma_sp;//errors for random effects
@@ -49,7 +55,7 @@ surv_mod2=stan(model_code="
   
   for (i in 1:N){
   
-  surv_mu[i]= inv_logit(alpha+beta1*mass[i]+beta2*death_type[i]+alpha_sp[Nsp]);
+  surv_mu[i]= inv_logit(alpha+beta1*mass[i]+beta2*death_type[i]+alpha_sp[Nsp]+alpha_st[Nst]);
   }
   
 
@@ -65,6 +71,7 @@ surv_mod2=stan(model_code="
   beta1~ normal (0,2.5);
   beta2~ normal (0,2.5);
   sigma_sp ~cauchy(0,2.5);
+  sigma_st~ cauchy(0,2.5);
   phi~ normal(0,2.5);
 
 
@@ -78,6 +85,11 @@ surv_mod2=stan(model_code="
   for(j in 1:Nsp){
            alpha_sp[j]~normal(alpha, sigma_sp);
   }
+  
+  for (f in 1: Nst){
+  
+          alpha_st[f]~normal(alpha, sigma_st);
+  }
   }
   generated quantities {
   
@@ -87,15 +99,21 @@ surv_mod2=stan(model_code="
     log_lik = beta_rng(A, B);
    
   }", data=list(N=N, survival=survival, mass=mass,death_type=death_type,
-                species=species, Nsp=40), chains=2, iter=500)
+                species=species,study=surv$stcode, Nst=77, Nsp=40), chains=2, iter=100)
 
 
 #model plot########
-post2=rstan::extract(surv_mod2)$log_lik
-mean(post2)
+PS3=readRDS("surv_model_ranspst.RDS")
+saveRDS(surv_mod3, file="surv_model_ranspst.RDS")
+post3=rstan::extract(surv_model_ranspst)$log_lik
+mean(post3)
 sp1=post2[,which(surv$spcode==1)] #matrix of one species
-matplot(t(sp1),type="l",col="grey",lty=1,xaxt="n", ylab = "survival estimate")
-mean_sp1=apply(post2,2,mean)
-lines(mean_sp1~c(1:252),col="red", lwd=2)
+matplot(post3,type="l",col="grey",lty=1,xaxt="n", ylab = "survival estimate")
+mean_sp3=apply(post3,2,mean)
+lines(mean_sp3~surv$Average.mass..kg.,col="red", lwd=2)
 sp11=subset(surv, spcode=="1")
-points(sp11, pch=21, cex=2, col="yellow", bg="yellow")
+points(surv$survival.est, pch=21, cex=1, col="red", bg="red")
+plot(mean_sp3~surv$Average.mass..kg.)
+plot(surv$survival.est~surv$Average.mass..kg.)
+mean(surv$survival.est) #0.765
+mean(mean_sp3) #0.637
